30946a95350ed757e238ef65a400c2ad95ba87da
ernestoabad11 <40123874+ernestoabad11@users.noreply.github.com>
2025-06-21 01:24:31 -0500

    q
    q

--- Stream_lit/Pagina_Principal.py
+++ Stream_lit/Pagina_Principal.py
@@ -34,7 +34,7 @@ def main():
     """)
     
     # Espacio reservado para una imagen gráfica
-    st.image("Imagenes/Muro_1.png", caption="Descripción gráfica del modelo", width=400)
+    st.image("ernestoabad11/gui-modelo-ml-curva-de-capacidad/main/Stream_lit/Imagenes/Muro_1.png", caption="Descripción gráfica del modelo", width=400)
 
 if __name__ == "__main__":
     main()
